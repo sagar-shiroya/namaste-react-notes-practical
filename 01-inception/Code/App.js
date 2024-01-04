c6b02e3c6817dc2d92ddda9eaa7439af5d0e65d8
Sagar Shiroya <sagar.shiroya8@gmail.com>
2024-01-04 10:24:32 +0530

    01 - Creating hierarchical complex DOM structure using react lib

--- 01-inception/Code/App.js
+++ 01-inception/Code/App.js
@@ -1,7 +1,28 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React!"
+// );
+// root.render(heading);
+
+/**
+ * For Complex DOM structure like:
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> I am an h1 tag</h1>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag")
+  )
+);
+console.log(parent);
+root.render(parent);
