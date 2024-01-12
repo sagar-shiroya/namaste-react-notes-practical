31279d03778592c1db377f023d93fbdcfb60915b
Sagar Shiroya <sagar.shiroya8@gmail.com>
2024-01-12 13:16:22 +0530

    01-Making HTML structure more complex and implement using createElement

--- 01-inception/Code/App.js
+++ 01-inception/Code/App.js
@@ -15,13 +15,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
  * </div>
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
     React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
-console.log(parent);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+
 root.render(parent);
