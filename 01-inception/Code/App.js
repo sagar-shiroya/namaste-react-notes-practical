d25b5d3476f607c1c50abae03183f23b0f30200a
Sagar Shiroya <sagar.shiroya8@gmail.com>
2024-01-12 13:14:36 +0530

    01-Adding sibling element using createElement

--- 01-inception/Code/App.js
+++ 01-inception/Code/App.js
@@ -18,11 +18,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am an h1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])
 );
 console.log(parent);
 root.render(parent);
